dcbb5a9a8127de8814439a5ebccdea15351a84d6
Siksss <matias.sotti@outlook.fr>
2025-07-11 08:00:04 +0200

    fichier requirements.txt

--- server/generateur.py
+++ server/generateur.py
@@ -13,7 +13,6 @@ def generateur(nbr_cote, nbr_rep, taille, angle, couleur, reduction=0.9):
     t.hideturtle()
     t.speed(0)
     t.pencolor(couleur)
-
     for i in range(nbr_rep):  #pour chaque rep la taille diminue et l'angle est modifie
         current_taille = taille * (reduction ** i)
         current_angle = angle * i
@@ -34,17 +33,15 @@ def generateur(nbr_cote, nbr_rep, taille, angle, couleur, reduction=0.9):
     filename = f"temp_{uuid.uuid4().hex}.ps"
     canvas = screen.getcanvas()
     canvas.postscript(file=filename, colormode='color')
-
     turtle.bye()
     #conversion du PostScript en PNG
     with Image.open(filename) as image:
         output = io.BytesIO()
         image.save(output, format="PNG")
         output.seek(0)
-
     os.remove(filename)
-
     return output
+
 #meme chose que generateur mais sans plusieurs repetitions
 def generateurFS(nbr_cote, taille, angle, couleur, reduction=0.9):
     turtle.TurtleScreen._RUNNING = True
@@ -60,26 +57,20 @@ def generateurFS(nbr_cote, taille, angle, couleur, reduction=0.9):
         x = taille * math.cos(theta)
         y = taille * math.sin(theta)
         coords.append((x, y))
-
     t.penup()
     t.goto(coords[0])
     t.pendown()
     for (x, y) in coords[1:]:
         t.goto(x, y)
-
     filename = f"temp_{uuid.uuid4().hex}.ps"
     canvas = screen.getcanvas()
     canvas.postscript(file=filename, colormode='color')
-
     turtle.bye()
-
     with Image.open(filename) as image:
         output = io.BytesIO()
         image.save(output, format="PNG")
         output.seek(0)
-
     os.remove(filename)
-
     return output
 
 
